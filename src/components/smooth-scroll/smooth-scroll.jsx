4d051a131e4760f3e38aa29a96e74437e4869045
Md Owakeel Ahmmed <96741142+owakeelAhmmed@users.noreply.github.com>
2026-05-20 12:34:11 +0600

    add lenis for smooth scrolling

---
+++ src/components/smooth-scroll/smooth-scroll.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+const SmoothScroll = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+      smoothTouch: false,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return null;
+};
+
+export default SmoothScroll;
