d05bae63f890ff57dfaaa3723bd03e9c62e113a6
Md Owakeel Ahmmed <96741142+owakeelAhmmed@users.noreply.github.com>
2026-07-07 16:18:57 +0600

    fix auth issue in dashboard

--- src/components/verifyemail/VerifyEmailPage.jsx
+++ src/components/verifyemail/VerifyEmailPage.jsx
@@ -21,9 +21,7 @@ export default function VerifyEmailPage() {
       }
 
       try {
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/api/auth/verify-email?token=${token}`
-        );
+        const response = await fetch(`/api/auth/verify-email?token=${token}`);
         const data = await response.json();
 
         if (data.success) {
